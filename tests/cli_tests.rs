06bcd9ab7ee0cee4513d7c375ee1a2b27e1839e2
Danielle Jenkins <danielle@megacrit.com>
2025-12-22 16:28:13 -0800

    Update deps and rename binary to sentry

--- tests/cli_tests.rs
+++ tests/cli_tests.rs
@@ -2,7 +2,7 @@ use assert_cmd::Command;
 use predicates::prelude::*;
 
 fn sentry_cli() -> Command {
-    Command::new(assert_cmd::cargo::cargo_bin!("sentry-cli"))
+    Command::new(assert_cmd::cargo::cargo_bin!("sentry"))
 }
 
 #[test]
@@ -22,7 +22,7 @@ fn test_version() {
         .arg("--version")
         .assert()
         .success()
-        .stdout(predicate::str::contains("sentry-cli"));
+        .stdout(predicate::str::contains("sentry"));
 }
 
 #[test]
